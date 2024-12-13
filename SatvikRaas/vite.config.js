e23f4cc627ba77f288decbcfad8d65a56a54ce0b
haraay <haraaydeployment@gmail.com>
2024-12-13 19:36:25 +0530

    Update vite.config.js

--- SatvikRaas/vite.config.js
+++ SatvikRaas/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": "http://15.207.46.61:8080", // Proxy to the HTTP API server
+    },
+  },
+});
