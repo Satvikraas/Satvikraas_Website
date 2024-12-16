fa3117364e88ca430517d08fb003b831b6f124e8
haraay <haraaydeployment@gmail.com>
2024-12-16 14:52:33 +0530

    added

--- SatvikRaas/src/api.jsx
+++ SatvikRaas/src/api.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8080"
-  baseURL: "https://15.207.46.61:443",
+  // baseURL: "https://15.207.46.61:443",
+  baseURL: "https://api.satvikraas.com:443",
 
   // , // Backend URL
   withCredentials: true, // Allows cookies to be sent
