fb5d2678cb5f11d519f28c4292f3c914d2249ba9
haraay <haraaydeployment@gmail.com>
2024-12-16 16:31:29 +0530

    added

--- SatvikRaas/src/Pages/Products/AllProducts.jsx
+++ SatvikRaas/src/Pages/Products/AllProducts.jsx
@@ -104,29 +104,43 @@ const Product = ({ product, filters }) => {
         alt={product.name}
       />
      */}
-      {/* IMP
-<div className={styles.variantButtons}>
+      IMP
+      <div className={styles.variantButtons}>
         {product.variants?.map((variant, index) => (
           <button
             key={index}
-            className={variant === selectedVariant ? styles.active : ''}
+            className={variant === selectedVariant ? styles.active : ""}
             onClick={() => handleVariantChange(variant)}
           >
             {variant.weight} g
-          </button> 
-        ))}   <button onClick={handleAddToCart} disabled={loading} className={styles.addCartButton}>
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M292.31-115.38q-25.31 0-42.66-17.35-17.34-17.35-17.34-42.65 0-25.31 17.34-42.66 17.35-17.34 42.66-17.34 25.31 0 42.65 17.34 17.35 17.35 17.35 42.66 0 25.3-17.35 42.65-17.34 17.35-42.65 17.35Zm375.38 0q-25.31 0-42.65-17.35-17.35-17.35-17.35-42.65 0-25.31 17.35-42.66 17.34-17.34 42.65-17.34t42.66 17.34q17.34 17.35 17.34 42.66 0 25.3-17.34 42.65-17.35 17.35-42.66 17.35ZM235.23-740 342-515.38h265.38q6.93 0 12.31-3.47 5.39-3.46 9.23-9.61l104.62-190q4.61-8.46.77-15-3.85-6.54-13.08-6.54h-486Zm-19.54-40h520.77q26.08 0 39.23 21.27 13.16 21.27 1.39 43.81l-114.31 208.3q-8.69 14.62-22.58 22.93-13.88 8.31-30.5 8.31H324l-48.62 89.23q-6.15 9.23-.38 20 5.77 10.77 17.31 10.77h435.38v40H292.31q-35 0-52.23-29.5-17.23-29.5-.85-59.27l60.15-107.23L152.31-820H80v-40h97.69l38 80ZM342-515.38h280-280Z"/></svg>
-      </button>
-      </div> */}
+          </button>
+        ))}{" "}
+        <button
+          onClick={handleAddToCart}
+          disabled={loading}
+          className={styles.addCartButton}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#000000"
+          >
+            <path d="M292.31-115.38q-25.31 0-42.66-17.35-17.34-17.35-17.34-42.65 0-25.31 17.34-42.66 17.35-17.34 42.66-17.34 25.31 0 42.65 17.34 17.35 17.35 17.35 42.66 0 25.3-17.35 42.65-17.34 17.35-42.65 17.35Zm375.38 0q-25.31 0-42.65-17.35-17.35-17.35-17.35-42.65 0-25.31 17.35-42.66 17.34-17.34 42.65-17.34t42.66 17.34q17.34 17.35 17.34 42.66 0 25.3-17.34 42.65-17.35 17.35-42.66 17.35ZM235.23-740 342-515.38h265.38q6.93 0 12.31-3.47 5.39-3.46 9.23-9.61l104.62-190q4.61-8.46.77-15-3.85-6.54-13.08-6.54h-486Zm-19.54-40h520.77q26.08 0 39.23 21.27 13.16 21.27 1.39 43.81l-114.31 208.3q-8.69 14.62-22.58 22.93-13.88 8.31-30.5 8.31H324l-48.62 89.23q-6.15 9.23-.38 20 5.77 10.77 17.31 10.77h435.38v40H292.31q-35 0-52.23-29.5-17.23-29.5-.85-59.27l60.15-107.23L152.31-820H80v-40h97.69l38 80ZM342-515.38h280-280Z" />
+          </svg>
+        </button>
+      </div>
       {/* IMP */}
       <h3 className={styles.pname}>{product.name}</h3>
-      {/* {selectedVariant && (
+      {selectedVariant && (
         <>
           <p>Price: ₹ {selectedVariant.price}</p>
-          {selectedVariant.discount > 0 && <p>Discount: {selectedVariant.discount}%</p>}
+          {selectedVariant.discount > 0 && (
+            <p>Discount: {selectedVariant.discount}%</p>
+          )}
         </>
-      )} */}
-
+      )}
       <button className={styles.buyButton} onClick={handleBuyNow}>
         Buy Now
       </button>
